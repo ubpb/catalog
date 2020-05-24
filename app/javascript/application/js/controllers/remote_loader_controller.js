59bbb29f19b36dce4d7d6cd4df0b0b76a3940d7b
René Sprotte <renspr@gmail.com>
2020-05-24 16:43:26 +0200

    Remote loading controller.

---
+++ app/javascript/application/js/controllers/remote_loader_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "stimulus"
+import axios from "axios"
+
+export default class extends Controller {
+  static targets = [ "output" ]
+
+  connect() {
+    this.loadPartial()
+  }
+
+  loadPartial() {
+    const url = this.data.get("url")
+    axios.get(url)
+      .then(response => response.data)
+      .then(html => this.outputTarget.innerHTML = html)
+      .catch(error => this.outputTarget.innerHTML = error)
+  }
+}

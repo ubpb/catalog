163e13bd83bf1bf9ed8d08760497f5ad5adbe49c
René Sprotte <renspr@gmail.com>
2020-05-24 16:43:00 +0200

    Update index.js

--- app/javascript/application/js/index.js
+++ app/javascript/application/js/index.js
@@ -1,13 +1,15 @@
-// Load / init stimulus.js and constrollers in ./controllers
+// Load / init stimulus.js and controllers in ./controllers
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-const application = Application.start()
+
 const context = require.context("./controllers", true, /\.js$/)
+const application = Application.start()
 application.load(definitionsFromContext(context))
 
 // Load / init axios
 import axios from "axios"
 const csrfToken = document.querySelector("meta[name=csrf-token]").content
+
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken
 document.addEventListener("turbolinks:render", () => {

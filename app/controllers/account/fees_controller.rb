449a67926d94491cb62a44a16ffc7ea5faa86795
René Sprotte <renspr@gmail.com>
2020-06-10 11:07:58 +0200

    UI tweeks.

--- app/controllers/account/fees_controller.rb
+++ app/controllers/account/fees_controller.rb
@@ -2,8 +2,14 @@ class Account::FeesController < Account::ApplicationController
 
   def index
     if request.xhr?
+      # Load fees
       fees = load_fees
+      # Sort by date desc
+      fees = fees.sort{|x,y| y.date <=> x.date}
+      # Calc the fees sum
       sum = fees.sum{|f| f.balance}
+
+      # Render fees listing
       render partial: "fees", locals: {
         sum: sum,
         fees: fees

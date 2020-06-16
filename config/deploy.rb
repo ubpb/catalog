3a77eac026f84bdca8e1ad1a19e17743376d9fc8
René Sprotte <renspr@gmail.com>
2020-06-16 11:34:04 +0200

    Add capistrano deployment.

---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+lock "~> 3.11"
+
+set :application, "catalog"
+set :repo_url, "git@github.com:ubpb/catalog.git"
+set :branch, "master"
+set :log_level, :debug
+
+append :linked_files, "config/database.yml", "config/master.key", "config/ils.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+set :rvm_type, :user
+set :rvm_ruby_version, IO.read(".ruby-version").strip
+
+set :passenger_roles, :web
+
+set :rails_env, "production"

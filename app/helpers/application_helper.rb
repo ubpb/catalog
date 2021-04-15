f68c6049f4a5f7d8684d3e2c56652f61ef485c56
René Sprotte <renspr@gmail.com>
2021-04-15 20:25:26 +0200

    Add active_when helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def active_when(regexp_path_or_boolean)
+    if regexp_path_or_boolean.is_a?(Regexp)
+      regexp = regexp_path_or_boolean
+      request.path =~ regexp ? "active" : ""
+    elsif regexp_path_or_boolean.is_a?(String)
+      path = regexp_path_or_boolean
+      request.path == path ? "active" : ""
+    else
+      bool = regexp_path_or_boolean
+      bool == true ? "active" : ""
+    end
+  end
+
 end

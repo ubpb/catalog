468dd1d3b9aba3a8237af5fe251b7bf12057b1cf
René Sprotte <renspr@gmail.com>
2021-04-21 11:17:15 +0200

    For the Ils "default" scope the adapter methods can now be called on the class.

--- app/integrations/ils.rb
+++ app/integrations/ils.rb
@@ -4,4 +4,12 @@ class Ils < BaseIntegration
     super(scope, config_filename, environment)
   end
 
+  def self.method_missing(method, *args)
+    if self[:default]
+      self[:default].send(method, *args)
+    else
+      super
+    end
+  end
+
 end

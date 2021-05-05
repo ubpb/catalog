427cdf434998c9883abf0c7cf65b584dea0eb7e5
René Sprotte <renspr@gmail.com>
2021-05-05 12:41:26 +0200

    I18n

--- app/controllers/account/application_controller.rb
+++ app/controllers/account/application_controller.rb
@@ -1,7 +1,7 @@
 class Account::ApplicationController < ApplicationController
 
   before_action :authenticate!
-  before_action { add_breadcrumb "Mein Bibliothekskonto", account_root_path }
+  before_action { add_breadcrumb t("account.application.breadcrumb"), account_root_path }
 
   layout "account/application"
 

1d9e3ff692702bea13079b2fc764a68045369ab1
René Sprotte <renspr@gmail.com>
2021-05-20 21:09:43 +0200

    Use our custom paginator.

--- app/integrations/ils/get_loans_result.rb
+++ app/integrations/ils/get_loans_result.rb
@@ -1,7 +1,7 @@
 class Ils
   class GetLoansResult < BaseStruct
     attribute :loans, Types::Array.of(Loan).default([].freeze)
-    attribute :total_number_of_loans, Ils::Types::Integer.default(0)
+    attribute :total, Ils::Types::Integer.default(0)
     attribute :page, Ils::Types::Integer.optional
     attribute :per_page, Ils::Types::Integer.optional
   end

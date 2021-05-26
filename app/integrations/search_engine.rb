8f2cc2399599dfd46783330ee6e512ab24efe0fc
René Sprotte <renspr@gmail.com>
2021-05-26 14:38:28 +0200

    Tweeks.

--- app/integrations/search_engine.rb
+++ app/integrations/search_engine.rb
@@ -1,5 +1,7 @@
 class SearchEngine < BaseIntegration
 
+  class Error < IntegrationError ; end
+
   def self.[](scope, config_filename = "#{Rails.root}/config/search_engine.yml", environment = Rails.env)
     super(scope, config_filename, environment)
   end

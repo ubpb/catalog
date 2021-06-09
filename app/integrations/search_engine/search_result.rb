a8de61ecf037bd1c9ea2d2707964bfb326ff9325
René Sprotte <renspr@gmail.com>
2021-06-09 14:03:37 +0200

    Add basic facet model.

--- app/integrations/search_engine/search_result.rb
+++ app/integrations/search_engine/search_result.rb
@@ -1,8 +1,9 @@
 class SearchEngine
   class SearchResult < BaseStruct
-    attribute :hits, Types::Array.of(Hit).default([].freeze)
-    attribute :total, Types::Integer.default(0)
-    attribute :page, Types::Integer.optional
+    attribute :hits,     Types::Array.of(Hit).default([].freeze)
+    attribute :facets,   Types::Array.of(Facets::BaseFacet).default([].freeze)
+    attribute :total,    Types::Integer.default(0)
+    attribute :page,     Types::Integer.optional
     attribute :per_page, Types::Integer.optional
   end
 end

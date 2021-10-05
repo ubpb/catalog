5ae1fbad6f791253cf06e3685a9401a93684765e
René Sprotte <renspr@gmail.com>
2021-10-05 11:25:29 +0200

    Add compatibility for v1 records.

---
+++ app/controllers/compat/application_controller.rb
@@ -0,0 +1,2 @@
+class Compat::ApplicationController < ApplicationController
+end
---
+++ app/controllers/compat/v1_records_controller.rb
@@ -0,0 +1,15 @@
+class Compat::V1RecordsController < Compat::ApplicationController
+
+  def show
+    record_id = params[:id]
+
+    if record_id.upcase.starts_with?("PAD_ALEPH")
+      # Local record
+      redirect_to(record_path(id: record_id.gsub(/PAD_ALEPH/i, ""), search_scope: "local"))
+    else
+      # CDI record
+      redirect_to(record_path(id: record_id, search_scope: "cdi"))
+    end
+  end
+
+end

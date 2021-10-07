5dadfe1935bf4efdffc208ec223bb92a4862e934
René Sprotte <renspr@gmail.com>
2021-10-07 16:42:00 +0200

    Format

--- app/controllers/compat/v1_records_controller.rb
+++ app/controllers/compat/v1_records_controller.rb
@@ -3,11 +3,9 @@ class Compat::V1RecordsController < Compat::ApplicationController
   def show
     record_id = params[:id]
 
-    if record_id.upcase.starts_with?("PAD_ALEPH")
-      # Local record
+    if record_id.upcase.starts_with?("PAD_ALEPH") # Local record
       redirect_to(record_path(id: record_id.gsub(/PAD_ALEPH/i, ""), search_scope: "local"))
-    else
-      # CDI record
+    else # CDI record
       redirect_to(record_path(id: record_id, search_scope: "cdi"))
     end
   end

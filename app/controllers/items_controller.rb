362b4e8ea7a97ec6edcbce9d33423bf1b929eb17
René Sprotte <renspr@gmail.com>
2021-10-08 14:35:01 +0200

    Use some random records for testing.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,17 @@
 class ItemsController < ApplicationController
 
+  TEST_RECORDS = [
+    "99943610000541",
+    "9944643600649",
+    "991195150000541",
+    "99926630000541",
+    "991450290000541",
+    "991379170000541"
+  ]
+
   def index
-    record_id = "9944643600649" # Fix ID from Sandbox for testing
+    record_id = TEST_RECORDS.sample # TODO: Use real record id
+
     @items = Ils.get_items(record_id)
   end
 

5a88e09f9e9ae3abc6380474a20816fbd46aea7a
René Sprotte <renspr@gmail.com>
2021-10-14 13:33:16 +0200

    Use fake IDs from Alma sandbox for items for now.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,17 @@
 class ItemsController < ApplicationController
 
   TEST_RECORDS = [
-   # "99943610000541",
+    "99943610000541",
     "9944643600649",
-   # "991195150000541",
-   # "99926630000541",
-   # "991450290000541",
-   # "991379170000541"
+    "991195150000541",
+    "99926630000541",
+    "991450290000541",
+    "991379170000541"
   ]
 
   def index
     @record_id = params[:record_id]
-    @items     = Ils.get_items(TEST_RECORDS.sample) # TODO: Use real record id
+    @items     = Ils.get_items(TEST_RECORDS.sample) # TODO: Remove fake ID
   end
 
 end

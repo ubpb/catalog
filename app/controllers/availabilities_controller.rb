a2c71e19635dc278018c542c66214741dda09c39
René Sprotte <renspr@gmail.com>
2021-10-14 15:45:46 +0200

    Add badge mode to availability controller.

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -4,9 +4,14 @@ class AvailabilitiesController < ApplicationController
     availabilities = Ils.get_availabilities(clean_record_ids)
 
     respond_to do |format|
-      format.json {
-        render json: get_availabilities_result(availabilities)
-      }
+      format.json do
+        case params[:mode]
+        when /badge/i then
+          render json: badge_result(availabilities)
+        else
+          render json: full_result(availabilities)
+        end
+      end
     end
   end
 
@@ -20,14 +25,41 @@ private
       .compact
   end
 
-  def get_availabilities_result(availabilities)
+  def calculated_availability(availabilities)
+    if availabilities[:availabilities].any?{|a| a[:availability] == "available"}
+      "available"
+    elsif availabilities[:availabilities].any?{|a| a[:availability] == "restricted"}
+      "restricted"
+    else
+      "unavailable"
+    end
+  end
+
+  def full_result(availabilities)
     availabilities.map do |availabilities|
       {
-        availabilities: availabilities,
+        record_id: availabilities[:record_id],
+        #availabilities: availabilities,
         html_content: render_to_string(
-          partial: "availabilities/availabilities",
+          partial: "availabilities/full",
           formats: :html,
           locals: {availabilities: availabilities}
+        )
+      }
+    end
+  end
+
+  def badge_result(availabilities)
+    availabilities.map do |availabilities|
+      availability = calculated_availability(availabilities)
+
+      {
+        record_id: availabilities[:record_id],
+        #availability: availability,
+        html_content: render_to_string(
+          partial: "availabilities/badge",
+          formats: :html,
+          locals: {availability: availability}
         )
       }
     end

326d9f68edc85bbe61e6ae89b5a2c1144d0cd3b8
René Sprotte <renspr@gmail.com>
2021-10-14 15:45:02 +0200

    Use given record id.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
 
   def index
     @record_id = params[:record_id]
-    @items     = Ils.get_items(TEST_RECORDS.sample) # TODO: Remove fake ID
+    @items     = Ils.get_items(@record_id)
   end
 
 end

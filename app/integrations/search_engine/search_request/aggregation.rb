e1c7e7ef2e874062beec8cc6ef324cc799582d07
René Sprotte <renspr@gmail.com>
2021-10-26 17:25:50 +0200

    Refactor the way search requests gets validated.

--- app/integrations/search_engine/search_request/aggregation.rb
+++ app/integrations/search_engine/search_request/aggregation.rb
@@ -1,7 +1,21 @@
 class SearchEngine
   class SearchRequest
 
-    class Aggregation < BasePart ; end
+    class Aggregation < BasePart
+
+      def validate!(adapter)
+        aggregation = adapter.aggregations.find{|s| s[:name] == self.name}
+
+        if self.value.blank?
+          return nil
+        elsif aggregation.nil?
+          return nil
+        else
+          self
+        end
+      end
+
+    end
 
   end
 end

56bce425b8c88480422f2649a3c369bd78449e2b
René Sprotte <renspr@gmail.com>
2022-01-26 14:09:57 +0100

    Remove fake record ids.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,5 @@
 class ItemsController < ApplicationController
 
-  TEST_RECORDS = [
-    "99943610000541",
-    "9944643600649",
-    "991195150000541",
-    "99926630000541",
-    "991450290000541",
-    "991379170000541"
-  ]
-
   def index
     @record_id = params[:record_id]
     @items     = Ils.get_items(@record_id)

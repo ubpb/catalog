5235729b92abf8f1e150fb0f5df16598eea0a66e
René Sprotte <renspr@gmail.com>
2022-03-14 10:06:49 +0100

    Use default username and no pasword.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_144830) do
-
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2022_01_06_144830) do
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "email"

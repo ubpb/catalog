df66ccec7d016fb3f7b911340ab7877c4a5bdf31
René Sprotte <renspr@users.noreply.github.com>
2022-05-31 10:30:45 +0200

    Add resource_type, material_type and content_type

--- app/integrations/search_engine/record.rb
+++ app/integrations/search_engine/record.rb
@@ -10,6 +10,13 @@ class SearchEngine
     # Is the record a superorder
     attribute :is_superorder, Types::Bool.default(false)
 
+    # Resource type
+    attribute :resource_type, Types::String.default("unspecified")
+    # Material type
+    attribute :material_type, Types::String.default("unspecified")
+    # Content type
+    attribute :content_type, Types::String.default("unspecified")
+
     # Unique ID of the record. REQUIRED.
     attribute :id, Types::String
     # Old Aleph ID
@@ -34,12 +41,11 @@ class SearchEngine
     attribute :edition, Types::String.optional
     # Physical description
     attribute :physical_description, Types::String.optional
-
     # Languages
     attribute :languages, Types::Array.of(Types::String).default([].freeze)
     # Subjects
     attribute :subjects, Types::Array.of(Types::String).default([].freeze)
-    # Lokal notations
+    # Local notations
     attribute :local_notations, Types::Array.of(Types::String).default([].freeze)
     # Notes
     attribute :notes, Types::Array.of(Types::String).default([].freeze)
@@ -67,63 +73,18 @@ class SearchEngine
     # the locally available volumes of that journal.
     attribute :journal_stocks, Types::Array.of(JournalStock).default([].freeze)
 
-    # # Local selection codes (Selektionskennzeichen)
-    # attribute :selection_codes, Types::Array.of(Types::String).default([].freeze)
-    # # Local signature (Signatur)
-    # attribute :signature, Types::String.optional
-    # # Local notations (Systemstellen)
-    # attribute :notations, Types::Array.of(Types::String).default([].freeze)
 
-    # # Content type (e.g. bibliography, textbook, dissertation, ...)
-    # attribute :content_type, Types::Symbol.default(:other)
-    # # Media type (e.g. monograph, journal, newspaper, ...)
-    # attribute :media_type, Types::Symbol.default(:other)
-    # # Carrier type (e.g. print, online_resource, data_storage, ...)
-    # attribute :carrier_type, Types::Symbol.default(:other)
+    def is_superorder?
+      self.is_superorder
+    end
 
-    # # Is the record a superorder record (Überordnung)?
-    # attribute :is_superorder, Types::Bool.default(false)
-    # # Is the record a secondary form record (Sekundärform)
-    # attribute :is_secondary_form, Types::Bool.default(false)
+    def is_online_resource?
+      self.is_online_resource
+    end
 
-    # # The title of the record. REQUIRED.
-    # attribute :title, Types::String
-    # # Creators and contributors
-    # attribute :creators_and_contributors, Types::Array.of(Types::String).default([].freeze)
-    # # Year of publication
-    # attribute :year_of_publication, Types::String.optional
-    # # Edition of the record
-    # attribute :edition, Types::String.optional
-    # # List of publishers
-    # attribute :publishers, Types::Array.of(Types::String).default([].freeze)
-    # # Format info
-    # attribute :format, Types::String.optional
-    # # List of languages (3 letter language codes)
-    # attribute :languages, Types::Array.of(Types::String).default([].freeze)
-    # # List of additional identifiers (e.g. ISBN, ISSN, etc.)
-    # attribute :identifiers, Types::Array.of(Identifier).default([].freeze)
-    # # List of subject terms
-    # attribute :subjects, Types::Array.of(Types::String).default([].freeze)
-    # # List of descriptions
-    # attribute :descriptions, Types::Array.of(Types::String).default([].freeze)
-    # # List of notes
-    # attribute :notes, Types::Array.of(Types::String).default([].freeze)
+    def is_deleted?
+      self.is_deleted
+    end
 
-    # # Link to link resolver
-    # attribute :resolver_link, ResolverLink.optional
-    # # List of links to online ressources for the record (TOCs, Thumbnails, etc.)
-    # #attribute :resource_links, Types::Array.of(Link).default([].freeze)
-    # # List of links to fulltexts of the record
-    # attribute :fulltext_links, Types::Array.of(Link).default([].freeze)
-
-    # # List of relations to other records
-    # attribute :part_of, Types::Array.of(Relation).default([].freeze)
-    # # A relation to another record that is the source of this record
-    # attribute :source, Relation.optional
-
-
-    # # A list of journal records, describing the available journal stock if
-    # # this record is a journal.
-    # attribute :journal_stocks, Types::Array.of(Journal).default([].freeze)
   end
 end

ba1fc3532973d24968d1b2f1c0f3880ede1fc646
René Sprotte <renspr@users.noreply.github.com>
2022-06-27 11:07:18 +0200

    Remove resource sharing holds from "normal" ones.

--- app/controllers/account/hold_requests_controller.rb
+++ app/controllers/account/hold_requests_controller.rb
@@ -4,14 +4,8 @@ class Account::HoldRequestsController < Account::ApplicationController
 
   def index
     if request.xhr?
-      # Load hold requests
-      hold_requests = load_hold_requests
-      # Sort by request date desc
-      hold_requests = hold_requests.sort{|x,y| y.requested_at <=> x.requested_at}
-
-      # Render hold request listing
       render partial: "hold_requests", locals: {
-        hold_requests: hold_requests
+        hold_requests: load_hold_requests
       }
     else
       render :index
@@ -33,7 +27,11 @@ private
   def load_hold_requests
     Ils.get_hold_requests(
       current_user.ils_primary_id
-    )
+    ).select do |hr|
+      hr.is_resource_sharing_request == false
+    end.sort do |x, y|
+      y.requested_at <=> x.requested_at
+    end
   end
 
   def delete_hold_request(request_id)

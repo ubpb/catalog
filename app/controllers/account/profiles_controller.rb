2eea4e63da1d49c2a4cc90faaefecebdae149104
René Sprotte <renspr@users.noreply.github.com>
2022-07-06 11:43:13 +0200

    Update the user before showing the profile.

--- app/controllers/account/profiles_controller.rb
+++ app/controllers/account/profiles_controller.rb
@@ -3,6 +3,9 @@ class Account::ProfilesController < Account::ApplicationController
   before_action { add_breadcrumb t("account.profiles.breadcrumb"), account_profile_path }
 
   def show
+    ils_user = Ils.get_user(current_user.ils_primary_id)
+    User.create_or_update_from_ils_user!(ils_user)
+    current_user.reload
   end
 
 end

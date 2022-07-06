3264fc81ce3695a536e3a8ebe966c6af72b33918
René Sprotte <renspr@users.noreply.github.com>
2022-07-06 11:46:44 +0200

    Add change email feature.

---
+++ app/controllers/account/emails_controller.rb
@@ -0,0 +1,41 @@
+class Account::EmailsController < Account::ApplicationController
+
+  before_action { add_breadcrumb t("account.emails.breadcrumb"), edit_account_email_path }
+  before_action :authorize
+
+  def edit
+    @form = EmailChangeForm.new
+  end
+
+  def update
+    @form = EmailChangeForm.new(
+      params.require(:email_change_form).permit(:current_password, :email)
+    )
+
+    if @form.valid?
+      if Ils.authenticate_user(current_user.ils_primary_id, @form.current_password)
+        # Form is valid and user is authenticated
+        # Try to change the email.
+        if Ils.set_user_email(current_user.ils_primary_id, @form.email)
+          flash[:success] = t(".success")
+          redirect_to account_profile_path
+        else
+          flash[:error] = t(".error")
+          redirect_to account_profile_path
+        end
+      else
+        @form.errors.add(:current_password, t(".current_password_is_wrong"))
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def authorize
+    authorize! :change_email, current_user
+  end
+
+end
---
+++ app/integrations/ils/adapters/alma_adapter/set_user_email_operation.rb
@@ -0,0 +1,35 @@
+module Ils::Adapters
+  class AlmaAdapter
+    class SetUserEmailOperation < Operation
+
+      def call(user_id, new_email)
+        # Get the details for the user
+        user = adapter.api.get("users/#{user_id}")
+        emails = user.dig("contact_info", "email")
+
+        # Find preferred E-Mail
+        preferred_email = emails.find do |email|
+          email["preferred"] == true
+        end
+
+        if preferred_email.present?
+          preferred_email["email_address"] = new_email
+        else
+          emails << {
+            "email_address": new_email,
+            "preferred": true,
+            "description": nil,
+            "segment_type"=>"Internal",
+            "email_type"=>[{"value"=>"personal"}]
+          }
+        end
+
+        adapter.api.put("users/#{user_id}", body: user.to_json, format: "application/json")
+        true
+      rescue ExlApi::LogicalError
+        false
+      end
+
+    end
+  end
+end

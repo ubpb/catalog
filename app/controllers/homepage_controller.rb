0bbd0297b31398dea0ae9337dbcd992630e816ae
René Sprotte <renspr@users.noreply.github.com>
2022-07-25 21:37:38 +0200

    Add blog news to the homepage.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,4 +2,8 @@ class HomepageController < ApplicationController
 
   before_action { add_breadcrumb t("homepage.breadcrumb"), root_path }
 
+  def show
+    @news = FeedParser.parse_rss_feed("https://blogs.uni-paderborn.de/ub-katalog?feed=rss")
+  end
+
 end

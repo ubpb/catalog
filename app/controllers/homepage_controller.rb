2e304d54a15dfe1540d4b06d4b6665b2dcb8256a
René Sprotte <renspr@users.noreply.github.com>
2022-08-01 10:13:29 +0200

    Add q shortcut.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,6 +3,14 @@ class HomepageController < ApplicationController
   before_action { add_breadcrumb t("homepage.breadcrumb"), root_path }
 
   def show
+    if params[:q].present?
+      sr = SearchEngine::SearchRequest.parse("sr[q,any]=#{params[:q]}")
+
+      return redirect_to(
+        new_search_request_path(sr)
+      )
+    end
+
     @news = FeedParser.parse_rss_feed("https://blogs.uni-paderborn.de/ub-katalog?feed=rss")
   end
 

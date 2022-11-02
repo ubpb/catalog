81be322d347f9a7b72fde558c70cebca224507a8
René Sprotte <renspr@users.noreply.github.com>
2022-11-02 15:29:43 +0100

    Take holdings from related records into account.

--- app/integrations/ils/adapters/alma_adapter/get_holdings_operation.rb
+++ app/integrations/ils/adapters/alma_adapter/get_holdings_operation.rb
@@ -12,18 +12,24 @@ module Ils::Adapters
     private
 
       def get_holdings(record_id)
-        # load holdings from Alma
-        response = load_holdings(record_id)
+        # Load holdings from Alma for the given record id
+        holdings = load_holdings(record_id)
 
-        # Build array of holding objects
-        holdings = []
-        holdings += response["holding"] || []
+        # Check if there are related records
+        related_record_ids = load_related_record_ids(record_id)
+
+        # If there are related records, load their holdings as well
+        if related_record_ids.present?
+          related_record_ids.each do |rrid|
+            holdings += load_holdings(rrid)
+          end
+        end
 
         # Filter out holdings with we don't want in discovery
         holdings = holdings.reject do |h|
           # Unassigned holdings
           h.dig("location", "value") =~ /UNASSIGNED/ ||
-          # Items that are suppressed from publishing
+          # Holdings that are suppressed from publishing
           # For whatever reason this boolean flag is of type string.
           h.dig("suppress_from_publishing") == "true" ||
           # Holdings at location LL (Ausgesondert)
@@ -35,9 +41,37 @@ module Ils::Adapters
         adapter.api.get(
           "bibs/#{record_id}/holdings",
           format: "application/json"
-        )
+        ).try(:[], "holding") || []
       rescue
-        {}
+        []
+      end
+
+      def load_related_record_ids(record_id)
+        related_record_ids = []
+
+        # Load the bibs information from Alma
+        bibs = adapter.api.get(
+          "bibs",
+          format: "application/xml",
+          params: {
+            mms_id: record_id,
+            expand: "p_avail", # Important to get the AVA fields
+            view: "full"
+          }
+        ).xpath("/bibs/bib")
+
+        # Extract record IDs
+        record_ids = bibs.map do |bib|
+          bib.xpath("//datafield[@tag='AVA']/subfield[@code='0']").text
+        end
+
+        # Remove the given record id from the list, so that only related record ids
+        # are left and make sure the ids are unique.
+        related_record_ids = record_ids.reject{|e| e == record_id}.uniq
+
+        related_record_ids
+      rescue
+        []
       end
 
     end

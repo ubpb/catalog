c279b317fec092f2a142b0508a87b777ba97bdda
René Sprotte <renspr@users.noreply.github.com>
2022-12-06 10:40:45 +0100

    Merge fulltext_links and resolver_link for local resources but be compatible with CDI needs.
    This mainly completes ubpb/issues#54 but we will tweek it a little further.

--- app/integrations/search_engine/link.rb
+++ app/integrations/search_engine/link.rb
@@ -2,5 +2,7 @@ class SearchEngine
   class Link < BaseStruct
     attribute :label, Types::String.optional
     attribute :url, Types::String
+    attribute :coverage, Types::String.optional
+    attribute :note, Types::String.optional
   end
 end

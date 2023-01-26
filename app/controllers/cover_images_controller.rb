b74b2395e6118a2bd895abed75d48104843bd6cd
petzimm <121883156+petzimm@users.noreply.github.com>
2023-01-26 13:11:47 +0100

    Revert "move logic to cover_images_controller"
    This reverts commit 8f90f911e6b057d70fada82951b8b410ec04a5af.

--- app/controllers/cover_images_controller.rb
+++ app/controllers/cover_images_controller.rb
@@ -8,28 +8,19 @@ class CoverImagesController < ActionController::Base
   READ_TIMEOUT = Config[:cover_images, :read_timeout, default: 0.5]
 
   def show
-    search_scope = params[:search_scope]
-    id = params[:id]
+    id   = params[:id]
     size = params[:size].presence || "m"
-    
-    # Load record by the given id
-    record = SearchEngine[search_scope].get_record(id)
 
-    isbn13 = record.additional_identifiers.select{|i| i.type == :isbn && i.value&.length >= 13}.first&.value&.gsub("-", "")&.delete(" ")
-
-    if isbn13
-      response = Rails.cache.fetch("cover-#{isbn13}-#{size}", expires_in: 24.hours) do
-        load_cover_image(isbn13, size: size)
-      end
-
-      if response
-        send_data response.body, disposition: "inline", type: response.content_type
-        return
-      end
+    response = Rails.cache.fetch("cover-#{id}-#{size}", expires_in: 24.hours) do
+      load_cover_image(id, size: size)
     end
 
-    # transparent gif
-    send_data Base64.decode64("R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="), disposition: "inline", type: "image/gif"
+    if response
+      send_data response.body, disposition: "inline", type: response.content_type
+    else
+      # transparent gif
+      send_data Base64.decode64("R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="), disposition: "inline", type: "image/gif"
+    end
   end
 
 private

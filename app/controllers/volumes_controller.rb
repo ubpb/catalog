a3bde5f4102631c4bfc2455e0b2af7c4dc5d7e64
René Sprotte <renspr@users.noreply.github.com>
2023-05-03 11:14:10 +0200

    Link to volumes using HBZ ID or ZDB ID.

--- app/controllers/volumes_controller.rb
+++ app/controllers/volumes_controller.rb
@@ -1,25 +1,29 @@
 class VolumesController < RecordsController
 
   def self.search_request(record)
-    # TODO: If superorder_id contains an Alma ID, linking using hbz_id will not work.
-    # We need to fix this in normalisation. This will only become a problem if Alma is the leading
-    # "Verbundsystem". Therefore we have some time to fix this.
-    sr_to_volumes = SearchEngine::SearchRequest.parse("sr[q,superorder_id]=#{record.hbz_id}&sr[s,asc]=volume")
+    # The superorder_id of the volumes is always the hbz_id or the zdb_id of the given record.
+    # If zdb_id is present we will link using the zdb_id, otherwise we will link using the hbz_id.
+    superorder_id = record.zdb_id.presence || record.hbz_id.presence || ""
+
+    SearchEngine::SearchRequest.parse("sr[q,superorder_id]=#{superorder_id}&sr[s,asc]=volume")
   end
 
   def index
-    add_breadcrumb(t(".breadcrumb"), record_volumes_path(
-      search_scope: current_search_scope,
-      record_id: @record.id
-    ))
+    add_breadcrumb(
+      t(".breadcrumb"),
+      record_volumes_path(
+        search_scope: current_search_scope,
+        record_id:    @record.id
+      )
+    )
 
     results = SearchEngine[current_search_scope].search(VolumesController.search_request(@record))
 
-    if results && results.total > 0
-      @volumes = true
-    else
-      @volumes = false
-    end
+    @has_volumes = if results&.total&.positive?
+                     true
+                   else
+                     false
+                   end
   end
 
 end

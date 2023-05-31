dba911503f9424bd00f6005b5a83ace7bfc66b55
René Sprotte <renspr@users.noreply.github.com>
2023-05-31 14:13:19 +0200

    Text tweeks and typos.

--- app/mailers/registrations_mailer.rb
+++ app/mailers/registrations_mailer.rb
@@ -2,7 +2,7 @@ class RegistrationsMailer < ApplicationMailer
 
   def notify_user(registrations)
     @registration = registrations
-    mail(to: @registration.email, subject: "[UB Paderborn] Ihre Registrierung")
+    mail(to: @registration.email, subject: "[UB Paderborn] Ihre Registrierung für einen Bibliotheksausweis")
   end
 
 end

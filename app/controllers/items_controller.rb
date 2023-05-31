7a4ee54e53e7dbcf6c6cdbba797a916a357d1c98
René Sprotte <renspr@users.noreply.github.com>
2023-05-31 17:15:25 +0200

    Fix sorting
    Closes ubpb/issues#96

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < RecordsController
-
   def index
     @return_uri = sanitize_return_uri(params[:return_uri])
 
@@ -8,7 +7,7 @@ class ItemsController < RecordsController
       record_id: @record.id
     ))
 
-    @items  = []
+    @items = []
     @items += Ils.get_items(@record.id)
     @items += Ils.get_items(params[:host_item_id]) if params[:host_item_id].present?
 
@@ -19,7 +18,7 @@ class ItemsController < RecordsController
       # holdings for this journal Stücktitel record and pick the first one with a journal signature.
       # TODO: Are there cases where more than one holding exists for the journal Stücktitel.
       holdings_for_journal_stücktitel = Ils.get_holdings(@record.id)
-      holdings_for_journal_stücktitel = holdings_for_journal_stücktitel.select{|h| journal_call_number?(h.call_number)}
+      holdings_for_journal_stücktitel = holdings_for_journal_stücktitel.select { |h| journal_call_number?(h.call_number) }
       holdings_for_journal_stücktitel = augment_locations(holdings_for_journal_stücktitel)
       @holding_for_journal_stücktitel = holdings_for_journal_stücktitel.first
       # For the reference to the journal we pick the first "is part of" reference.
@@ -43,17 +42,17 @@ class ItemsController < RecordsController
         # "n.yyyy", "n.yyyy,m" and "yyyy,n" and make them sortable.
         @items = @items.map do |item|
           sort_key = case item.description
-                     # n.yyyy,m
-                     when /(\d{1,2})\.(\d{4}),(\d{1,2})/ then "#{$2.rjust(4, '0')}-#{$1.rjust(4, '0')}-#{$3.rjust(4, '0')}"
-                     # n.yyyy
-                     when /(\d{1,2})\.(\d{4})/           then "#{$2.rjust(4, '0')}-#{$1.rjust(4, '0')}-0000"
-                     # yyyy,n
-                     when /(\d{4}),(\d{1,2})/            then "#{$1.rjust(4, '0')}-#{$2.rjust(4, '0')}-0000"
-                     # yyyy
-                     when /(\d{4})/                      then "#{$1.rjust(4, '0')}-0000-0000"
-                     # ...
-                     else item.description
-                     end
+          # n.yyyy,m
+          when /(\d{1,4})\.(\d{4}),(\d{1,2})/ then "#{$2.rjust(4, "0")}-#{$1.rjust(4, "0")}-#{$3.rjust(4, "0")}"
+          # n.yyyy
+          when /(\d{1,4})\.(\d{4})/ then "#{$2.rjust(4, "0")}-#{$1.rjust(4, "0")}-0000"
+          # yyyy,n
+          when /(\d{4}),(\d{1,4})/ then "#{$1.rjust(4, "0")}-#{$2.rjust(4, "0")}-0000"
+          # yyyy
+          when /(\d{4})/ then "#{$1.rjust(4, "0")}-0000-0000"
+          # ...
+          else item.description
+          end
 
           item.new(sort_key:)
         end
@@ -68,14 +67,14 @@ class ItemsController < RecordsController
       # Prepare items for display...
       if @items.present?
         # Sort items by call number. Handle nil case for call number (sorted last)
-        @items = @items.sort_by{|item| [item.call_number ? 1 : 0, item.call_number]}
+        @items = @items.sort_by { |item| [item.call_number ? 1 : 0, item.call_number] }
 
         # Augment item location label with data from the static location lookup table
         @items = augment_locations(@items)
 
         # Item stats
         @no_of_items = @items.count
-        @no_of_available_items = @items.count{|i| i.is_available == true}
+        @no_of_available_items = @items.count { |i| i.is_available == true }
 
         # Get hold requests for that record
         @hold_requests = Ils.get_hold_requests_for_record(@record.id)
@@ -97,11 +96,11 @@ class ItemsController < RecordsController
   end
 
   def semapp_location
-    @item_id  = params[:id]
+    @item_id = params[:id]
     @username = Ils.get_semapp_location(@record.id, @item_id)
   end
 
-private
+  private
 
   def augment_locations(items_or_holdings)
     items_or_holdings.each do |ioh|
@@ -110,15 +109,12 @@ private
           if (jls = journal_locations(ioh.call_number, ioh.location.code)).present?
             ioh.location.attributes[:label] = jls.join("; ")
           end
-        else
-          if (ml = mono_location(ioh.call_number, ioh.location.code)).present?
-            ioh.location.attributes[:label] = ml
-          end
+        elsif (ml = mono_location(ioh.call_number, ioh.location.code)).present?
+          ioh.location.attributes[:label] = ml
         end
       end
     end
 
     items_or_holdings
   end
-
 end

575bbec4474edfad4011e23de24456359a41929e
René Sprotte <renspr@users.noreply.github.com>
2023-11-23 15:38:36 +0100

    First eID-Card implemenation

---
+++ app/forms/pin_change_form.rb
@@ -0,0 +1,19 @@
+class PinChangeForm
+  include ActiveModel::API
+
+  attr_accessor :current_pin
+  attr_accessor :new_pin
+  attr_accessor :new_pin_confirmation
+  attr_accessor :pin_in_ils
+
+  validates :current_pin, presence: true
+  validates :new_pin, presence: true, confirmation: true, length: {minimum: 4, maximum: 8}
+  validate :current_pin_is_correct
+
+  def current_pin_is_correct
+    if pin_in_ils != current_pin
+      errors.add(:current_pin, "ist nicht korrekt.")
+    end
+  end
+
+end
--- app/integrations/ils/user.rb
+++ app/integrations/ils/user.rb
@@ -7,5 +7,24 @@ class Ils
     attribute :email, Ils::Types::String.optional
     attribute :notes, Types::Array.of(Ils::Types::String).optional
     attribute :force_password_change, Ils::Types::Bool.default(false)
+    attribute :barcode, Ils::Types::String.optional
+    attribute :pin, Ils::Types::String.optional
+
+    def has_pin_set?
+      pin.present?
+    end
+
+    def full_name
+      [first_name, last_name].map(&:presence).compact.join(" ")
+    end
+
+    def full_name_reversed
+      [last_name, first_name].map(&:presence).compact.join(", ")
+    end
+
+    def short_barcode
+      barcode&.at(0..-3)
+    end
+
   end
 end

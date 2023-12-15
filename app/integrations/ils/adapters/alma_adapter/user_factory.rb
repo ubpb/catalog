2576bf19af3aa92fea41595252be7164c39739d7
René Sprotte <renspr@users.noreply.github.com>
2023-12-15 11:22:13 +0100

    Cleanup.

--- app/integrations/ils/adapters/alma_adapter/user_factory.rb
+++ app/integrations/ils/adapters/alma_adapter/user_factory.rb
@@ -3,7 +3,7 @@ module Ils::Adapters
     class UserFactory
 
       def self.build(alma_user_hash)
-        self.new.build(alma_user_hash)
+        new.build(alma_user_hash)
       end
 
       def build(alma_user_hash)
@@ -21,7 +21,7 @@ module Ils::Adapters
         )
       end
 
-    private
+      private
 
       def get_id(alma_user_hash)
         alma_user_hash["primary_id"]
@@ -46,9 +46,7 @@ module Ils::Adapters
 
       def get_email(alma_user_hash)
         emails = alma_user_hash["contact_info"]["email"]
-        emails.find{ |email| email["preferred"] == true }.try(:[], "email_address")
-        #emails.find(->{{}}){|email| email["preferred"] == true}["email_address"] ||
-        #  emails&.first.try(:[], "email_address")
+        emails.find { |email| email["preferred"] == true }.try(:[], "email_address")
       end
 
       def get_nodes(alma_user_hash)

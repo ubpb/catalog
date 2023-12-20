543f87c02b38139c76ec2f2b2eca9e7028356431
René Sprotte <renspr@users.noreply.github.com>
2023-12-20 14:07:23 +0100

    Tweek.

--- app/controllers/account/application_controller.rb
+++ app/controllers/account/application_controller.rb
@@ -1,8 +1,8 @@
 class Account::ApplicationController < ApplicationController
 
   before_action :authenticate!
-  before_action { add_breadcrumb t("account.application.breadcrumb"), account_root_path }
   before_action :check_activation
+  before_action { add_breadcrumb t("account.application.breadcrumb"), account_root_path }
 
   layout "account/application"
 

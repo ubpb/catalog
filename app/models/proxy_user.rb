f15d48dde3edcc13bdfba2dcb26403a5b2f20f5f
René Sprotte <renspr@users.noreply.github.com>
2024-01-16 11:42:23 +0100

    Add basic CRUD operations for the proxy user feature.

--- app/models/proxy_user.rb
+++ app/models/proxy_user.rb
@@ -1,8 +1,11 @@
-class ProxyUser < ApplicationUser
+class ProxyUser < ApplicationRecord
 
   belongs_to :user
 
-  validates :ils_primary_id, presence: true
-  validates :label, presence: true
+  validates :ils_primary_id, presence: true, uniqueness: {scope: :user_id} # rubocop:disable Rails/UniqueValidationWithoutIndex
+  validates :name, presence: true
+
+  # Not stored in the database, but used to lookup the proxy user in the ILS
+  attribute :barcode, :string
 
 end

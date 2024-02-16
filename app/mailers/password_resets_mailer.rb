47538de4f61fb8d729002612f25c6c909c90d6e8
René Sprotte <renspr@users.noreply.github.com>
2024-02-16 08:42:29 +0100

    I18n for password reset mail.

--- app/mailers/password_resets_mailer.rb
+++ app/mailers/password_resets_mailer.rb
@@ -2,7 +2,7 @@ class PasswordResetsMailer < ApplicationMailer
 
   def notify_user(user)
     @user = user
-    mail(to: @user.ils_user.email, subject: "[UB Paderborn] Passwort zurücksetzen")
+    mail(to: @user.ils_user.email)
   end
 
 end

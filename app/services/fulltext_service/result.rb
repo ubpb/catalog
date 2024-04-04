623ec2321f0df421d6bb9707713eeb34faa863d7
René Sprotte <renspr@users.noreply.github.com>
2024-04-04 17:06:59 +0200

    Add browzine support.

--- app/services/fulltext_service/result.rb
+++ app/services/fulltext_service/result.rb
@@ -1,13 +1,14 @@
 class FulltextService::Result
 
-  attr_reader :source, :url, :label, :coverage, :note
+  attr_reader :source, :url, :label, :coverage, :note, :options
 
-  def initialize(source:, url:, label: nil, coverage: nil, note: nil)
+  def initialize(source:, url:, label: nil, coverage: nil, note: nil, options: {})
     @source = source
     @url = url
     @label = label
     @coverage = coverage
     @note = note
+    @options = options
   end
 
 end

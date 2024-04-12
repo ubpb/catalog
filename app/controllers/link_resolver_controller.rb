cdc96633ead357c6e29511548566a6818834ead2
René Sprotte <renspr@users.noreply.github.com>
2024-04-12 16:02:59 +0200

    Further work on link resolver rewrite (WIP)

--- app/controllers/link_resolver_controller.rb
+++ app/controllers/link_resolver_controller.rb
@@ -6,6 +6,10 @@ class LinkResolverController < ApplicationController
     # Check Open URL against Alma link Resolver if Open URL params present
     # in the request.
     @result = AlmaLinkResolverService.resolve(request.fullpath) # TODO: user_ip ?
+
+    # Check if we have an MMS-ID and try to load the record from our search index.
+    mms_id = @result&.context&.mms_id
+    @record = SearchEngine[:local].get_record(mms_id) if mms_id
   end
 
 end

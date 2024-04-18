a7c937bbac38d2b6bad53282775b9c2594fb8cf0
René Sprotte <renspr@users.noreply.github.com>
2024-04-18 09:33:41 +0200

    For further research links we need the alma link resolver context.

--- app/services/fulltext_service/results.rb
+++ app/services/fulltext_service/results.rb
@@ -1,11 +1,12 @@
 class FulltextService::Results
   include Enumerable
 
-  attr_reader :results, :service_had_timeout
+  attr_reader :results, :service_had_timeout, :alma_link_resolver_context
 
-  def initialize(results: [], service_had_timeout: false)
+  def initialize(results: [], service_had_timeout: false, alma_link_resolver_context: nil)
     @results = results
     @service_had_timeout = service_had_timeout
+    @alma_link_resolver_context = alma_link_resolver_context
   end
 
   alias_method :service_had_timeout?, :service_had_timeout

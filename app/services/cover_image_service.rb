80a8ac0f955be2c4f063b33c8bca49d8a228beab
René Sprotte <renspr@users.noreply.github.com>
2024-04-26 11:25:19 +0200

    Cache results and tweak API.

--- app/services/cover_image_service.rb
+++ app/services/cover_image_service.rb
@@ -1,7 +1,15 @@
 class CoverImageService < ApplicationService
 
+  ENABLED          = Config[:cover, :enabled, default: false]
+  TIMEOUT          = Config[:cover, :timeout, default: 2.0]
+  CACHE_EXPIRES_IN = Config[:cover, :cache_expires_in, default: 7.days]
+
   class << self
     delegate :resolve, to: :new
+
+    def enabled?
+      ENABLED
+    end
   end
 
   #
@@ -10,21 +18,25 @@ class CoverImageService < ApplicationService
   # @param [SearchEngine::Record] record The record from the search engine to find a cover image for.
   # @return [String] The cover image as data string.
   #
-  def resolve(record)
-    # Try LibKey
-    if LibKeyService.enabled?
-      url = LibKeyService.resolve_cover_image(record)
-      return cover_image(url) if url.present?
-    end
+  def resolve(record, force: false)
+    cache_key = Digest::MD5.hexdigest(record.id)
 
-    # Try VLB
-    if VlbService.enabled?
-      url = VlbService.resolve_cover_image(record)
-      return cover_image(url) if url.present?
-    end
+    Rails.cache.fetch("cover-image-#{cache_key}", expires_in: CACHE_EXPIRES_IN, force: force) do
+      # Try LibKey
+      if ENABLED && LibKeyService.enabled?
+        url = LibKeyService.resolve_cover_image(record)
+        return cover_image(url) if url.present?
+      end
 
-    # No cover image found
-    placeholder_image
+      # Try VLB
+      if ENABLED && VlbService.enabled?
+        url = VlbService.resolve_cover_image(record)
+        return cover_image(url) if url.present?
+      end
+
+      # No cover image found
+      placeholder_image
+    end
   end
 
   private
@@ -32,7 +44,8 @@ class CoverImageService < ApplicationService
   def cover_image(url)
     Faraday.new(
       request: {
-        timeout: 0.5
+        open_timeout: TIMEOUT,
+        timeout: TIMEOUT
       }
     ) do |faraday|
       faraday.response :raise_error

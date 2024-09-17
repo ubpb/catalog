fb2d3a6dcba8a7bc6b1551076eb448c093c141cf
René Sprotte <renspr@users.noreply.github.com>
2024-09-17 14:56:53 +0200

    Add direct DOI resolving via LibKey.

--- app/services/lib_key_service/result.rb
+++ app/services/lib_key_service/result.rb
@@ -1,12 +1,13 @@
 class LibKeyService::Result
 
-  attr_reader :url, :browzine_url, :retraction_notice_url, :cover_image_url
+  attr_reader :url, :browzine_url, :retraction_notice_url, :cover_image_url, :libkey_io_url
 
-  def initialize(url:, browzine_url: nil, retraction_notice_url: nil, cover_image_url: nil)
+  def initialize(url:, browzine_url: nil, retraction_notice_url: nil, cover_image_url: nil, libkey_io_url: nil)
     @url = url
     @browzine_url = browzine_url
     @retraction_notice_url = retraction_notice_url
     @cover_image_url = cover_image_url
+    @libkey_io_url = libkey_io_url
   end
 
 end

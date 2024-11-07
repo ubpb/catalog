59be78f9e3858318a118d3d7153bec4e3d870a91
René Sprotte <renspr@users.noreply.github.com>
2024-11-07 09:16:41 +0100

    Cleanup.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,12 @@ class SessionsController < ApplicationController
 
   def new
     @return_uri = sanitize_return_uri(params[:return_uri])
+    return unless current_user
 
-    if current_user
-      if @return_uri.present?
-        redirect_to @return_uri
-      else
-        redirect_to account_root_path
-      end
+    if @return_uri.present?
+      redirect_to return_uri
+    else
+      redirect_to account_root_path
     end
   end
 
@@ -21,29 +20,32 @@ class SessionsController < ApplicationController
 
     if user_id.present? && password.present?
       if Ils.authenticate_user(user_id, password)
+        # Load user from ILS.
         ils_user = Ils.get_user(user_id)
 
+        # Check if the user needs activation and redirect to activation page
+        # if needed.
         if ils_user.needs_activation?
           flash[:error] = t(".user_needs_activation_flash", activation_link: activation_root_path)
           redirect_to new_session_path and return
         end
 
+        # The user has authenticated successfully and does not need activation.
+        # Make sure to create or update the local user record if it does not exist yet.
         db_user = User.create_or_update_from_ils_user!(ils_user)
-        # Make sure we have the latest data from the ILS inside the user object.
-        # because the ils user data is cached and we want the new login to reset that cache.
-        db_user.reload_ils_user!
 
         # Login user
         setup_current_user_session(user_id: db_user.id)
 
+        # Finally, redirect the user.
         flash[:success] = t(".success")
-
         if @return_uri.present?
           redirect_to @return_uri
         else
           redirect_to account_root_path
         end
       else
+        # User failed to authenticate.
         flash[:error] = t(".error")
         render :new, status: :unprocessable_entity
       end

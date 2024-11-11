5d59ff4a29c12ad221441a554c716c28746bf973
René Sprotte <renspr@users.noreply.github.com>
2024-11-11 16:11:48 +0100

    Set Pin as part of the activation process.

--- app/forms/pin_form.rb
+++ app/forms/pin_form.rb
@@ -1,9 +1,5 @@
-class PinForm
+class PinForm < ApplicationForm
 
-  include ActiveModel::API
-
-  attr_accessor :pin, :pin_confirmation
-
-  validates :pin, presence: true, confirmation: true, length: {minimum: 4, maximum: 8}, format: {with: /\A[0-9]+\z/}
+  include PinFormBase
 
 end

0e77388436dd85a593a18e6cd948d84a3899436b
René Sprotte <renspr@users.noreply.github.com>
2024-11-12 16:35:54 +0100

    Only send mails if we have an email address.

--- app/mailers/proxy_users_mailer.rb
+++ app/mailers/proxy_users_mailer.rb
@@ -3,12 +3,17 @@ class ProxyUsersMailer < ApplicationMailer
   def proxy_user_created(proxy_user:)
     @proxy_user = proxy_user
 
-    mail(to: @proxy_user.proxy_user.ils_user.email)
+    to = @proxy_user.proxy_user.ils_user.email
+    return if to.blank?
+
+    mail(to: to)
   end
 
   def proxy_user_deleted(proxy_user_email:, proxy_user_name:, proxy_for_user_name: )
     @proxy_user_name = proxy_user_name
     @proxy_for_user_name = proxy_for_user_name
+
+    return if proxy_user_email.blank?
 
     mail(to: proxy_user_email)
   end
@@ -17,12 +22,16 @@ class ProxyUsersMailer < ApplicationMailer
     @proxy_user_name = proxy_user_name
     @proxy_for_user_name = proxy_for_user_name
 
+    return if proxy_user_email.blank?
+
     mail(to: proxy_user_email)
   end
 
   def proxy_user_expired_to_proxy_for_user(proxy_for_user_email:, proxy_user_name:, proxy_for_user_name:)
     @proxy_user_name = proxy_user_name
     @proxy_for_user_name = proxy_for_user_name
+
+    return if proxy_for_user_email.blank?
 
     mail(to: proxy_for_user_email)
   end
--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -2,27 +2,47 @@ class UsersMailer < ApplicationMailer
 
   def activation_request(user)
     @user = user
-    mail(to: @user.ils_user.email)
+
+    to = @user.ils_user.email
+    return if to.blank?
+
+    mail(to: to)
   end
 
   def account_activated(user)
     @user = user
-    mail(to: @user.ils_user.email)
+
+    to = @user.ils_user.email
+    return if to.blank?
+
+    mail(to: to)
   end
 
   def registration_request(registration_request)
     @registration_request = registration_request
-    mail(to: @registration_request.email)
+
+    to = @registration_request.email
+    return if to.blank?
+
+    mail(to: to)
   end
 
   def registration_created(registrations)
     @registration = registrations
-    mail(to: @registration.email)
+
+    to = @registration.email
+    return if to.blank?
+
+    mail(to: to)
   end
 
   def password_reset_request(user)
     @user = user
-    mail(to: @user.ils_user.email)
+
+    to = @user.ils_user.email
+    return if to.blank?
+
+    mail(to: to)
   end
 
 end
